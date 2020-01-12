547f1545ed33c12f0b4ce4d0717b5fc739a8e7e9
Zach Latta <zach@zachlatta.com>
2020-01-12 15:46:10 -0500

    Run rustfmt

--- 0123-formatting.rs
+++ 0123-formatting.rs
@@ -9,16 +9,28 @@ struct Color {
 
 impl fmt::Display for Color {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "RGB ({r}, {g}, {b}) 0x{r:02X}{g:02X}{b:02X}", r=self.r, g=self.g, b=self.b)
+        write!(
+            f,
+            "RGB ({r}, {g}, {b}) 0x{r:02X}{g:02X}{b:02X}",
+            r = self.r,
+            g = self.g,
+            b = self.b
+        )
     }
 }
 
 fn main() {
     for color in [
-        Color { r: 128, g: 255, b: 90},
+        Color {
+            r: 128,
+            g: 255,
+            b: 90,
+        },
         Color { r: 0, g: 3, b: 254 },
         Color { r: 0, g: 0, b: 0 },
-    ].iter() {
+    ]
+    .iter()
+    {
         println!("{}", color)
     }
 }
